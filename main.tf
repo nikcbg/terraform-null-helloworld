8324ff9a6c2d80aa3d82967b280536d2de43713b
nikcbg <nikcbg@yahoo.com>
2021-12-15 11:31:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  cloud {
+    organization = "nikolay-paid"
+
+    workspaces {
+      name = "terraform-null-helloworld"
+    }
+  }
+}
+
+
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo hello world"

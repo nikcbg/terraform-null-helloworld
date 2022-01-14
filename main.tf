55e7091162c23054033db6a83d9dd44c8ef92746
nikcbg <nikcbg@yahoo.com>
2022-01-14 11:47:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "nikolay-paid"
-
-    workspaces {
-      name = "terraform-null-helloworld"
-    }
-  }
-} 
-
-
-
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo hello world"
